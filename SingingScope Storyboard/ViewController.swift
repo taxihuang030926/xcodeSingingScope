881c6f63013b1bec8577895133127a2c24d55ea1
Ting Yu Huang <taxihuang030926@gmail.com>
2020-10-07 14:41:59 +0800

    Initial Commit

---
+++ SingingScope Storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SingingScope Storyboard
+//
+//  Created by 黃廷宇 on 2020/10/7.
+//  Copyright © 2020 2020 NEHS CTSP pbl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
